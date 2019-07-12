46825b61bd3c613247362771a23030a62c03a479
Bill Li <slaytraz@gmail.com>
2019-07-12 22:32:32 +1200

    Some Updates

--- LeagueLoader/LeagueLoader.py
+++ LeagueLoader/LeagueLoader.py
@@ -1,9 +1,24 @@
 import kivy
+kivy.require('1.0.6') # replace with your current kivy version !
 
-class MyApp(kivy.App):
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.config import Config
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+
+Config.set('graphics', 'fullscreen', 'fake')
+
+class MyApp(App):
 
     def build(self):
-        return Label(text='Hello world')
+        root = BoxLayout()
+
+        button = Button(text="Exit", background_color=[0, 0, 0, 0], size_hint=(None, None))
+        button.bind(on_press=exit)
+        return button
+
 
 if __name__ == '__main__':
     MyApp().run()
---
+++ php/test.php
@@ -0,0 +1,7 @@
+<pre>
+<?php
+    $records = @dns_get_record('billdev.co.nz');
+
+    print_r($records);
+    ?>
+        </pre>
